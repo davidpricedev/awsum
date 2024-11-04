6a6c593e32730cd42d9b0b1e90e14c8f03aa4d55
davidpricedev <davidpricedev@gmail.com>
2024-11-04 07:49:28 -0600

    fixes

--- test/commands/sso/info.test.ts
+++ test/commands/sso/info.test.ts
@@ -1,9 +1,11 @@
 import { runCommand } from "@oclif/test";
 import { expect } from "chai";
 
-describe("sso", () => {
-  it("runs sso", async () => {
-    const { stdout } = await runCommand("sso info");
-    expect(stdout).to.contain("ssoRole");
+describe("sso info", () => {
+  it("will contain userId field", async () => {
+    const { stdout, stderr } = await runCommand("sso info");
+    console.log("stdout: ", stdout);
+    console.log("stderr: ", stderr);
+    expect(stdout).to.contain("userId");
   });
 });
